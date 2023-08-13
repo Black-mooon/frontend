27381e1b52d9970e000750ac557d360073d36a75
Dibyendu Das <get2dibyendu@gmail.com>
2023-08-13 09:33:22 +0530

    basic landing page with dashboard

---
+++ components/Header/menuData.tsx
@@ -0,0 +1,13 @@
+import { Menu } from "@/types/menu";
+
+const menuData: Menu[] = [
+  {
+    id: 1,
+    title: "About Us",
+    path: "/about",
+    newTab: false,
+  },
+  
+  
+];
+export default menuData;
